8264aca5325a82ec905a59f06fda1cb3b3241e96
aqueel192@gmail.com <aqueel192@gmail.com>
2024-11-27 23:29:42 +0530

    fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { motion } from "framer-motion";
 import { Menu, X } from "lucide-react";
